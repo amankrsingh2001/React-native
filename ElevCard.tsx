09cb7d7e24f6116fff24b37e8de5497cc882a446
amankrsingh2001 <141403170+amankrsingh2001@users.noreply.github.com>
2023-09-10 14:13:28 +0530

    Add files via upload

---
+++ ElevCard.tsx
@@ -0,0 +1,65 @@
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function ElevCard() {
+  return (
+    <View>
+      <Text style = {styles.headingText}>ElevCard</Text>
+      <ScrollView horizontal ={true} style = {styles.container}>
+        <View style = {[styles.card,styles.cardElevated]}>
+            <Text>Tap</Text>
+        </View>
+        <View style = {[styles.card,styles.cardElevated]}>
+            <Text>Me</Text>
+        </View>
+        <View style = {[styles.card,styles.cardElevated]}>
+            <Text>To</Text>
+        </View>
+        <View style = {[styles.card,styles.cardElevated]}>
+            <Text>scroll</Text>
+        </View>
+        <View style = {[styles.card,styles.cardElevated]}>
+            <Text>more..</Text>
+        </View>
+        <View style = {[styles.card,styles.cardElevated]}>
+            <Text>😊</Text>
+        </View>
+      </ScrollView>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    headingText:{
+        fontSize:40,
+        fontWeight:'bold',
+        paddingHorizontal:10
+      }, 
+      container:{
+        padding:8,
+
+      },
+      card:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center',
+        width:100,
+        height:100,
+        borderRadius:4,
+        margin:8,
+    
+
+      },
+      cardElevated:{
+        backgroundColor:'#CAD5E2',
+        elevation:4,
+        shadowoffset:{
+            width:1,
+            height:1
+        },shadowColor:'#EF5354',
+        shadowOpacity:0.3,
+        shadowRadius:2,
+      },
+      
+     
+})
